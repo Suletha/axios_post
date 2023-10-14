5b97658426f872e95c7369f048427af6c87a1d53
suletha <suletha.gopi@gmail.com>
2023-10-14 16:11:22 +0530

    serv

--- main.js
+++ main.js
@@ -15,9 +15,19 @@ document.getElementById("submitButton").addEventListener("click", function () {
         phone: phone,
     };
 
+    axios.post("https://crudcrud.com/api/df7fb11a5e624d5e9f7d329df9251dcc/user_details",users)
+        .then((respone) => {
+            console.log(respone)
+        })
+        .catch((err) =>{
+
+        })
     // Add the user object to the array
     users.push(user);
 
+    // Save the updated user array to local storage
+    //localStorage.setItem("userDetails", JSON.stringify(users));
+ 
     // Display all user details on the screen
     const userDetailsDiv = document.getElementById("userDetails");
     userDetailsDiv.innerHTML = '';
@@ -27,7 +37,7 @@ document.getElementById("submitButton").addEventListener("click", function () {
             <h2>User ${index + 1} Details:</h2>
             <p>Name: ${user.name}</p>
             <p>Email: ${user.email}</p>
-            <p>Email: ${user.phone}</p>
+            <p>Phone: ${user.phone}</p>
         `;
     });
 });
